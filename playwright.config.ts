3473f0a4a84b7a3b793a91f46e726adb1ea0cd68
naopeke <naopeke@protonmail.com>
2024-09-13 12:37:58 +0200

    jsonReports

--- playwright.config.ts
+++ playwright.config.ts
@@ -21,7 +21,14 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  // reporter: 'html',
+  reporter: [["dot"], ["json", {
+    outputFile: "jsonReports/jsonReport.json"
+  }], ["html", {
+    open: "never"
+  }]
+  ],  
+  
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /*リアルタイムでブラウザが開くようheadlessとactionTimeoutを追加*/
