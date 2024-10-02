f7f54432d40e584c53046b319517ff7d1ba9ca86
naopeke <naopeke@protonmail.com>
2024-10-02 12:09:01 +0200

    cross browser

--- tests/loginWithCrossBrowser.test.ts
+++ tests/loginWithCrossBrowser.test.ts
@@ -1,8 +1,34 @@
-import { chromium, test } from "@playwright/test";
+import { chromium, test, ConnectOptions } from "@playwright/test";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+// LambdaTest capabilities
+const capabilities = {
+    browserName: "Chrome", // Browsers allowed: `Chrome`, `MicrosoftEdge`, `pw-chromium`, `pw-firefox` and `pw-webkit`
+    browserVersion: "latest",
+    "LT:Options": {
+      platform: "Windows 10",
+      build: "Playwright TS Build",
+      name: "Playwright Test",
+      user: process.env.LT_USERNAME,
+      accessKey: process.env.LT_ACCESS_KEY,
+      network: true,
+      video: true,
+      console: true,
+      tunnel: false, // Add tunnel configuration if testing locally hosted webpage
+      tunnelName: "", // Optional
+      geoLocation: '', // country code can be fetched from https://www.lambdatest.com/capabilities-generator/
+    },
+  };
 
 test("Login test demo", async () => {
 
-    const browser = await chromium.launch();
+    const browser = await chromium.connect(
+        `wss://cdp.lambdatest.com/playwright?capabilities=
+        ${encodeURIComponent(JSON.stringify(capabilities)
+        )}`,
+      );
     const context = await browser.newContext();
     const page = await context.newPage();
 
